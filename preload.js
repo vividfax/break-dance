cc275fe86238516845956c374c2bffee016abbdc
vividfax <vividfax@gmail.com>
2021-09-12 19:17:05 +0100

    Add smaller audio files
    Former-commit-id: 1a78ff331818bdf89abfc00c0c6d509cadb87b00

--- preload.js
+++ preload.js
@@ -13,32 +13,32 @@ function preload() {
 
     coffeeShop = loadImage("images/coffeeShop.png");
 
-    A = newAudio("A");
-    B = newAudio("B");
-    C = newAudio("C");
-    D = newAudio("D");
-    E = newAudio("E");
-    F = newAudio("F");
-    G = newAudio("G");
-    H = newAudio("H");
-    I = newAudio("I");
-    J = newAudio("J");
-    K = newAudio("K");
-    L = newAudio("L");
-    M = newAudio("M");
-    N = newAudio("N");
-    O = newAudio("O");
-    P = newAudio("P");
-    Q = newAudio("Q");
-    R = newAudio("R");
-    S = newAudio("S");
-    T = newAudio("T");
-    U = newAudio("U");
-    V = newAudio("V");
-    W = newAudio("W");
-    X = newAudio("X");
-    Y = newAudio("Y");
-    Z = newAudio("Z");
+    A = newAudio("A.m4a");
+    B = newAudio("B.m4a");
+    C = newAudio("C.m4a");
+    D = newAudio("D.m4a");
+    E = newAudio("E.wav");
+    F = newAudio("F.m4a");
+    G = newAudio("G.wav");
+    H = newAudio("H.m4a");
+    I = newAudio("I.wav");
+    J = newAudio("J.wav");
+    K = newAudio("K.wav");
+    L = newAudio("L.m4a");
+    M = newAudio("M.wav");
+    N = newAudio("N.m4a");
+    O = newAudio("O.m4a");
+    P = newAudio("P.m4a");
+    Q = newAudio("Q.wav");
+    R = newAudio("R.m4a");
+    S = newAudio("S.wav");
+    T = newAudio("T.m4a");
+    U = newAudio("U.wav");
+    V = newAudio("V.wav");
+    W = newAudio("W.wav");
+    X = newAudio("X.wav");
+    Y = newAudio("Y.wav");
+    Z = newAudio("Z.wav");
 }
 
 function newAudio(c) {
@@ -47,12 +47,12 @@ function newAudio(c) {
 
     for (let i = 0; i < 26; i++) {
         player = new Tone.Player({
-            url: "sounds/" + c + ".wav",
+            url: "sounds/" + c,
         }).toMaster();
     };
-    gains[c] = new Tone.Gain(-1);
-    player.connect(gains[c]);
-    gains[c].toMaster();
+    gains[c[0]] = new Tone.Gain(-1);
+    player.connect(gains[c[0]]);
+    gains[c[0]].toMaster();
 
     loop = new Tone.Loop((time) => {
         player.start();
